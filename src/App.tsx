c638825b95a722bb4c0577632900d9d29f0773ed
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-03 15:28:14 +0100

    dichiara i tipi Recipe e Chef

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import './App.css'
 
 function App() {
-  
-  async function getChefBirthday(id: number){
-    const recipeRes = await fetch(`https://dummyjson.com/recipes/${id}`);
-    const recipe: unknown = await recipeRes.json();
-    const chefRes = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
-    const chef: unknown = await chefRes.json();
-    
-    return chef.birthDate
+
+  type Recipe = {
+    userId: number;
   }
 
-  getChefBirthday(1)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
-  .catch(error => console.error("Errore:", error.message));
+  type Chef = {
+    birthDate: string
+  }
+
 
   return (
     <>
