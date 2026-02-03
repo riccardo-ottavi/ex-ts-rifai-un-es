2aeb7f1bb5b9569e528ce8a2ae314abca353e534
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-03 17:45:33 +0100

    inserisci type guards

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,42 @@ function App() {
     birthDate: string
   }
 
+  function isRecipe(data: unknown): data is Recipe{
+    return(
+      typeof data === "object" &&
+      data !== null &&
+      "userId" in data && typeof data.userId === "number" 
+    )
+  }
+
+  function isChef(data: unknown): data is Chef{
+    return(
+      typeof data === "object" &&
+      data !== null &&
+      "birthDate" in data && typeof data.birthDate === "string" 
+    )
+  }
+
   async function getChefBirthDay(recipeId: number): Promise<string | null> {
     try {
       const recipeRes = await fetch(`https://dummyjson.com/recipes/${recipeId}`)
       if(!recipeRes.ok){
         throw new Error("Errore HTTP")
       }
-      const recipeData = await recipeRes.json()
+      
+      const recipeData: unknown = await recipeRes.json()
+      if(!isRecipe(recipeData)){
+        throw new Error ("Formato dei dati non valido")
+      }
+
       const chefRes = await fetch(`https://dummyjson.com/users/${recipeData.userId}`)
       if(!chefRes.ok){
         throw new Error("Errore HTTP")
       }
       const chefData = await chefRes.json()
+      if(!isChef(chefData)){
+        throw new Error ("Formato dei dati non valido")
+      }
       console.log(chefData)
       return chefData.birthDate
     }catch(error){
