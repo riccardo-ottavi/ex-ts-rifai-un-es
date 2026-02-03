cf39aa305e894e95254e0127216916e8ab7d228f
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-03 17:08:24 +0100

    avvolgi la funzione con try/catch

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,24 @@ function App() {
     birthDate: string
   }
 
-  async function getChefBirthDay(recipeId: number): Promise<string>{
-    const recipeRes = await fetch(`https://dummyjson.com/recipes/${recipeId}`)
-    const recipeData = await recipeRes.json()
-    const chefRes = await fetch(`https://dummyjson.com/users/${recipeData.userId}`)
-    const chefData = await chefRes.json()
-    console.log(chefData)
-    return chefData.birthDate
+  async function getChefBirthDay(recipeId: number): Promise<string | null> {
+    try {
+      const recipeRes = await fetch(`https://dummyjson.com/recipes/${recipeId}`)
+      const recipeData = await recipeRes.json()
+      const chefRes = await fetch(`https://dummyjson.com/users/${recipeData.userId}`)
+      const chefData = await chefRes.json()
+      console.log(chefData)
+      return chefData.birthDate
+    }catch(error){
+      console.error(error) 
+      return null
+    }
+    
   }
 
   getChefBirthDay(1)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
-  .catch(error => console.error("Errore:", error.message));
+    .then(birthday => console.log("Data di nascita dello chef:", birthday))
+    .catch(error => console.error("Errore:", error.message));
 
   return (
     <>
