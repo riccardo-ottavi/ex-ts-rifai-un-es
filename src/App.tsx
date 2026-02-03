a671edaf5bbf8d2bb8545a22d25194ba933cbd34
riccardo-ottavi <ottavi.ps@libero.it>
2026-02-03 15:45:20 +0100

    inizializza funzione getChefBirthDay()

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,18 @@ function App() {
     birthDate: string
   }
 
+  async function getChefBirthDay(recipeId: number): Promise<string>{
+    const recipeRes = await fetch(`https://dummyjson.com/recipes/${recipeId}`)
+    const recipeData = await recipeRes.json()
+    const chefRes = await fetch(`https://dummyjson.com/users/${recipeData.userId}`)
+    const chefData = await chefRes.json()
+    console.log(chefData)
+    return chefData.birthDate
+  }
+
+  getChefBirthDay(1)
+  .then(birthday => console.log("Data di nascita dello chef:", birthday))
+  .catch(error => console.error("Errore:", error.message));
 
   return (
     <>
